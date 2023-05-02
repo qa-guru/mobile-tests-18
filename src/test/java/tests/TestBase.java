080301192eb4975a3035ab8aad1440ebb659c303
svasenkov <aanher@gmail.com>
2023-05-02 21:25:07 +0200

    added attachs

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,15 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import drivers.BrowserstackDriver;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestBase {
     @BeforeAll
@@ -18,12 +20,19 @@ public class TestBase {
 
     @BeforeEach
     void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         open();
     }
 
     @AfterEach
     void afterEach() {
+        String sessionId = sessionId().toString();
+//        Attach.screenshotAs("Last screenshot"); // todo
+        Attach.pageSource();
+
         closeWebDriver();
+
+        Attach.addVideo(sessionId);
     }
 
 }
